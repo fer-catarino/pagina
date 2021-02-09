d61d669151bfde5e9df99a64cf89602e6fd31a83
Fernanda Catarino <51006669+fer-catarino@users.noreply.github.com>
2021-02-09 09:56:02 -0600

    archivo

---
+++ App.vue
@@ -0,0 +1,125 @@
+<template>
+ 
+  <div class="bg-local ..." style="background-image: url('')"></div>
+  <div class=" bg-opacity-25 ..."></div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#273036" fill-opacity="1" d="M0,160L48,144C96,128,192,96,288,96C384,96,480,128,576,154.7C672,181,768,203,864,181.3C960,160,1056,96,1152,85.3C1248,75,1344,117,1392,138.7L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
+   
+<!-- Header nav -->
+<article class="container my-3 mx-10  md:flex justify-between items-center">
+<!-- Left -->
+    <section class="p-2 md:flex items-center">
+        <a href="#" class="block mb-9 md:mr-7">
+            <img class="mx-auto" src="./assets/logog.png"  width="100">
+        </a>
+    </section>
+
+    <!-- Right -->
+    <section class=" p-2 md:flex items-center">
+        <a href="#" class="bg-blue-500 block mb-7 md:mr-6">
+            <img class="mx-auto" src="./assets/Vector.png" alt="Escuela Vue" width="20">
+        </a>
+        <a href="#" class="nav-item active md:mb-9 md:mr-8">Industries</a>
+        <a href="#" class="nav-item md:mb-9 md:mr-8">Services</a>
+        <a href="#" class="nav-item md:mb-9 md:mr-8">About Us</a>
+        <a href="#" class="nav-item md:mb-9 md:mr-5">Contact</a>
+        <a href="#" class="nav-item md:mb-9 md:mr-2">
+            <img class="mx-auto" src="./assets/mx.png" alt="Escuela Vue" width="20">
+        </a>
+        <a href="#" class="nav-item md:mb-9 ">
+            <img class="mx-auto" src="./assets/usa.png" alt="Escuela Vue" width="20">
+        </a>
+    </section>
+</article>
+
+<!-- End Header nav -->
+
+<!-- texto y boton -->
+<article class="p-8 bg-blue my-20 w-20 h-2">
+    <p class="font-bold font-ubuntu mb-15 text-left text-blue-800 text-2xl mx-20">Providing Competitive Edges</p>
+</article>
+<section class="p-9 py-1 bg-blue w-15 mx-20 ">
+      <button class="bg-blue-400 hover:bg-blue-dark text-white text-xs py-2 px-2 rounded-full">
+        View Case Study
+      </button>
+    </section>
+<!-- Oferta -->
+<article class="p-15 bg-oscuro ">
+  <section class="md:flex text-justify">
+      <div class="px-24 py-20 bg-blue my-8 mx-20 ">
+        <div class="font-Ubuntu text-blue-900 text-xl mb-15 w-40">Our Offering</div>
+          <p class=" my-2 text-black text-xs">Headquartered in Austin Texas with a nearshore development center in Durango Mexico.</p>
+          <p class="text-black  text-xs">G-TECH provides, solutions and products to clients at a great value.</p>
+        </div>
+      <div class="py-24 mb-3 bg-white rounded-lg ml-0 mr-5">
+        <img class="offerings" src="./assets/herramientas.png" alt="herramientas" width="30">
+      <h2 class="font-bold text-justify my-3">Nearshore Development
+Center</h2>
+          <p class="text-gray-700 text-xs">Nearshore Development Center is superior to OnShore and Offshore, as it improves on cost-effectiveness while keeping the superior comunication and resoponse with our teams, keeping the same timezone with our clients.</p>
+</div>
+  <div class="py-24 mb-3 bg-white rounded-lg mr-3">
+            <img class="offerings" src="./assets/compu.png" alt="compu" width="30">
+            <h2 class="font-bold text-justify my-3">Latest technology</h2>
+            <p class="text-gray-700 text-xs">We employ the latest technology, practices and methodologies, all to offer exceptional councel, services, and products at a reasonable price. We bring extensive experience, professionalism and innovation to every project, service or product.</p>
+  </div>
+  <div class="py-24 bg-white rounded-lg">
+            <img class="offerings" src="./assets/foco.png" alt="Foco" width="30">
+            <h2 class="font-bold text-justify my-3">Delivery Services</h2>
+            <p class="text-gray-700 text-xs">Client will specify the expected delivery and G-TECH will execute the project and work towards deliver the required scope, on time and on budget.</p>
+        </div>
+    </section>
+</article>
+
+<!-- Testimonials -->
+<article class="p-15 bg-oscuro">
+    <section class="md:flex">
+        <div class="p-6 mb-3 ml-80 bg-white rounded-lg mr-3 ">
+            <img class="offerings" src="./assets/edificios.png" alt="edificios" width="30">
+            <h2 class="font-bold text-justify my-3">US based Professional Services</h2>
+            <p class="text-gray-700 text-xs">Senior IT Professionals with strong technical expertise in their respective areas bring their experience and skills to solve complex problems through technology.
+Advanced Software Services
+IT Consulting</p>
+        </div>
+        <div class="p-6 mb-3 bg-white rounded-lg mr-3">
+            <img class="offerings" src="./assets/equipo.png" alt="Equipo" width="30">
+            <h2 class="font-bold text-justify my-3">Research and Development</h2>
+            <p class="text-gray-700 text-xs">Our R&D teams are focused on improving the technologies we use for delivery activities, while also exploring potential disruptive technologies that can offer interesting niche opportunities.</p>
+        </div>
+        <div class="p-6 bg-white rounded-lg">
+            <img class="offerings" src="./assets/nube.png" alt="Nube" width="30">
+            <h2 class="font-bold text-justify my-3">Cloud Managed Services</h2>
+            <p class="text-gray-700 text-xs">G-TECH will execute the required to process, practices and task to ensure the Client's Cloud infrastructure is working in optimal level.
+Cloud Maintenance
+Proactive Monitoring and Alerting
+Agile Support
+DevOps Services</p>
+        </div>
+    </section>
+</article>
+<!-- End Testimonials -->
+
+<article class="p-15 bg-oscuro flex-left">
+    <section class="flex-left">
+        <div class="flex-left">
+<img class="float-left" src="./assets/io1.png" width="500">
+<p>We employ the latest technology, practices and methodologies, all to offer exceptional councel, services, and products at a reasonable price. We bring extensive experience, professionalism and innovation to every project, service or product, aiming to solve our clients problems, needs, and concerns.
+</p>
+        </div>
+        </section>
+</article>
+
+<article class="p-4 bg-oscuro flex-right mt-60">
+        <section class="flex-right">
+        <div class="flex-right">
+<img class="float-right" src="./assets/Graphic-Designer1.png" width="500">
+<p>We employ the latest technology, practices and methodologies, all to offer exceptional councel, services, and products at a reasonable price. We bring extensive experience, professionalism and innovation to every project, service or product, aiming to solve our clients problems, needs, and concerns.
+</p>
+        </div>
+    </section>
+</article>
+  
+  <router-view/>
+</template>
+
+<style>
+
+</style>
